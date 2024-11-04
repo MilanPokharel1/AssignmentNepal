44f36def7acf5233bf289a476bc3b5bd31c000bf
Sachet407 <sachetkhatiwada5901@gmail.com>
2024-11-04 00:56:50 -0800

    milan

---
+++ src/screens/client/Order/Components/FilterButtons.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+const FilterButtons = ({ activeFilter, onFilterChange }) => {
+  const buttons = ["All","Pending","Approved", "Ongoing","Submitted", "Completed"];
+
+  return (
+    <div className="flex gap-4 mt-4">
+      {buttons.map((button) => (
+        <button
+          key={button}
+          className={`px-4 py-2 rounded-lg ${
+            activeFilter === button
+              ? "bg-[#5d5fef] text-white"
+              : "bg-gray-100 text-gray-700"
+          } transition-colors`}
+          onClick={() => onFilterChange(button)}
+        >
+          {button}
+        </button>
+      ))}
+    </div>
+  );
+};
+
+export default FilterButtons;
