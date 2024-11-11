c649bcadabd97eefcaf47b5504d919609464a995
Sachet407 <sachetkhatiwada5901@gmail.com>
2024-11-10 21:57:26 -0800

    a bit more responsive

--- src/screens/client/Client.jsx
+++ src/screens/client/Client.jsx
@@ -2,13 +2,15 @@ import React from "react";
 import { Outlet } from "react-router-dom";
 import SideNavbar from "./ClientComponents/SideNavbar";
 import TopNavbar from "./ClientComponents/TopNavbar";
+
 const Client = () => {
   return (
     <div className="min-h-screen flex gap-2 bg-white">
-      <div className="SideNavHide:w-[19%] SideNavHide:h-screen SideNavHide:bg-gray-50 SideNavHide:fixed hidden SideNavHide:block">
+
+      <div className="w-0 SideNavHide:w-[19%] h-screen bg-gray-50 fixed invisible SideNavHide:visible">
         <SideNavbar />
       </div>
-      <div className="flex flex-1 flex-col w-full ">
+      <div className="flex flex-1 flex-col SideNavHide:ml-[20%] ml-[4vw]">
         <div className="h-[4rem]">
           <TopNavbar notificationCount={19} />
         </div>
