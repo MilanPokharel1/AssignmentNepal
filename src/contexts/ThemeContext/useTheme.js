4b2a4d7f7c2cacf4c0f186dab3afcdd4b96c5c8e
Sachet407 <sachetkhatiwada5901@gmail.com>
2024-11-11 23:50:43 -0800

    hello milan

--- src/contexts/ThemeContext/useTheme.js
+++ src/contexts/ThemeContext/useTheme.js
@@ -1,7 +1,7 @@
 import { useContext } from "react";
 import { ThemeContext } from "./ThemeContext";
 
-export const useTheme = () => {
+export const UseTheme = () => {
   const context = useContext(ThemeContext);
   if (!context) {
     throw new Error("useTheme must be used within a ThemeProvider");
