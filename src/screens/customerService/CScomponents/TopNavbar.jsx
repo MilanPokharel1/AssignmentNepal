8b513b188ece806962d95d5cd8caad5f7540e1b4
Sachet407 <sachetkhatiwada5901@gmail.com>
2024-12-10 04:57:15 -0800

    milan

--- src/screens/customerService/CScomponents/TopNavbar.jsx
+++ src/screens/customerService/CScomponents/TopNavbar.jsx
@@ -60,7 +60,7 @@ const TopNavbar = ({
       <div className="flex items-center space-x-4">
         <div className="relative w-auto h-auto p-1 rounded-2xl bg-yellow-100">
           <button
-            onClick={() => navigate("/client/reminders")}
+            onClick={() => navigate("/cs/reminders")}
             className="p-2 hover:bg-gray-100 rounded-full transition-colors"
           >
             <MdOutlineNotifications className="h-5 w-5 text-yellow-600" />
